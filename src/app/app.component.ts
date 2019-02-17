787320725259481c8149df25a75adcca0cf749b5
JAEHO SONG <thruthesky@gmail.com>
2019-02-18 01:37:40 +0800

    adding tab 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  menus = [
+    { title: 'Evie & Co 인사말', icon: 'home', url: '/' },
+    { title: '갤러리', icon: 'photos', url: '/tabs/tab2' },
+    { title: '서비스 및 비용 안내', icon: 'help', url: '/tabs/tab5' },
+    { title: '연락처', icon: 'help', url: '/' },
+    { title: '오시는길', icon: 'help', url: '/' },
+    { title: '홈페이지', icon: 'help', url: '/' },
+    { title: '페이스북', icon: 'help', url: '/' }
+];
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -24,3 +33,4 @@ export class AppComponent {
     });
   }
 }
+
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage {
+  constructor(
+    private menuController: MenuController
+  ) {
+  }
+  onClickMenu() {
+    console.log('menu clicked');
+    this.menuController.open();
+  }
+}
