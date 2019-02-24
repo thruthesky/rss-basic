8a7b0c4ef90c6b1ba389d91bd13e8e8b92b679cd
JAEHO SONG <thruthesky@gmail.com>
2019-02-24 17:17:26 +0800

    update for tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SonubAppLibraryModule } from 'sonub-app-library/sonub-app-library.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    SonubAppLibraryModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tabs.router.module.ts
+++ src/app/tabs.router.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TabsPage } from './tabs.page';
+import { TabsPage } from './tabs/tabs.page';
 
 const routes: Routes = [
   {
